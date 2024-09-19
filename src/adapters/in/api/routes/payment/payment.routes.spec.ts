4f36cc5773e1000fc9bd1606749430cd6d30dfb1
Razvan Negrea <rznart@gmail.com>
2024-09-19 12:48:33 +0200

    Add integration tests for /payment

---
+++ src/adapters/in/api/routes/payment/payment.routes.spec.ts
@@ -0,0 +1,28 @@
+import request from "supertest"
+import { beforeEach, describe, it, expect, vi } from "vitest"
+
+// Helpers
+import app from "@mocks/app.mocks.js"
+
+beforeEach(() => {
+  vi.resetAllMocks()
+})
+
+describe("GET /payment", () => {
+  describe("given the ENV var is set up correctly", () => {
+    it("should respond with the Stripe PK", async () => {
+      const res = await request(app).get("/payment").send()
+
+      // Verify base response
+      expect(res.status).toBe(200)
+      expect(res.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      )
+
+      // Verify data integrity
+      expect(res.body).toHaveProperty("ok", true)
+
+      expect(res.body.data.publishable_key).toBeDefined()
+    })
+  })
+})
